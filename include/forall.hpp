8531cf17d6a75e42806ad6cd5950986e73a757a2
arotem3 <arotem33@gmail.com>
2024-04-22 19:28:08 -0400

    Adding cuda

---
+++ include/forall.hpp
@@ -0,0 +1,24 @@
+#ifndef CUDDH_FORALL_HPP
+#define CUDDH_FORALL_HPP
+
+#include <cuda_runtime.h>
+
+template <typename LAMBDA>
+void forall(int n, LAMBDA fun)
+{
+    const int block_size = 256;
+    const int n_blocks = (n + block_size - 1) / block_size;
+
+    forall_kernel<<< n_blocks, block_size >>>(n, fun);
+}
+
+template <typename LAMBDA>
+__global__ static void forall_kernel(int n, LAMBDA fun)
+{
+    const int k = threadIdx.x + blockIdx.x * blockDim.x;
+    if (k >= n) return;
+    
+    fun(k);
+}
+
+#endif
