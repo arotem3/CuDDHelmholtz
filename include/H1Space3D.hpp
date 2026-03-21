16dbe00fb2a253073be7d73f2c33b291e468d92c
arotem3 <arotem33@gmail.com>
2026-03-20 23:47:56 -0400

    formatting

--- include/H1Space3D.hpp
+++ include/H1Space3D.hpp
@@ -17,10 +17,7 @@ namespace cuddh
 
         /// @brief returns the dimension of the space, i.e. the number of
         /// degrees of freedom.
-        int size() const
-        {
-            return ndof;
-        }
+        int size() const { return ndof; }
 
         /// @brief returns the inidices of the global degrees of freedom from
         /// element local indicies.
@@ -31,23 +28,14 @@ namespace cuddh
         }
 
         /// @brief returns a reference to the mesh
-        const Mesh3D &mesh() const
-        {
-            return _mesh;
-        }
+        const Mesh3D &mesh() const { return _mesh; }
 
         /// @brief returns a reference to the basis set
-        const Basis &basis() const
-        {
-            return _basis;
-        }
+        const Basis &basis() const { return _basis; }
 
         /// @brief returns the physical coordinates corresponding to collocation
         /// point of each nodal DOF. The output has shape (ndof,).
-        VectorWrapper<const double3> physical_coordinates(MemorySpace m) const
-        {
-            return reshape(_xyz.read(m), ndof);
-        }
+        VectorWrapper<const double3> physical_coordinates(MemorySpace m) const { return reshape(_xyz.read(m), ndof); }
 
     private:
         const int n_elem;
@@ -75,6 +63,8 @@ namespace cuddh
             double3 xi = x[i];
             F[i] = f(xi);
         });
+
+        CUDDH_CUDA_CHECK(cudaDeviceSynchronize());
     }
 
     class TraceSpace3D
@@ -84,40 +74,25 @@ namespace cuddh
         TraceSpace3D(const H1Space3D &fem, int n_faces, const int *faces);
 
         /// @brief returns the dimension of the space
-        int size() const
-        {
-            return ndof;
-        }
+        int size() const { return ndof; }
 
         /// @brief returns the number of faces in the space
-        int n_faces() const
-        {
-            return nf;
-        }
+        int n_faces() const { return nf; }
 
         /// @brief returns the face indices of the faces in the space
-        const_ivec_wrapper faces(MemorySpace m) const
-        {
-            return reshape(_faces.read(m), nf);
-        }
+        const_ivec_wrapper faces(MemorySpace m) const { return reshape(_faces.read(m), nf); }
 
         /// @brief returns the indices of the TraceSpace3D degrees of freedom
         /// corresponding to the local face indices. Specifically,
         /// subspace_indices(i, j, f) is the subspace index of the (i,j) basis
         /// function on face f. These indices range from 0 to this->size()-1.
-        const_icube_wrapper subspace_indices(MemorySpace m) const
-        {
-            return reshape(_I.read(m), n_basis, n_basis, nf);
-        }
+        const_icube_wrapper subspace_indices(MemorySpace m) const { return reshape(_I.read(m), n_basis, n_basis, nf); }
 
         /// @brief returns the indicies of the global degrees of freedom in the
         /// H1Space3D relative to the TraceSpace3D. that is, global_indicies(i) is
         /// the index in H1Space3D corresponding to the i-th TraceSpace3D degree of
         /// freedom.
-        const_ivec_wrapper global_indices(MemorySpace m) const
-        {
-            return reshape(_proj.read(m), ndof);
-        }
+        const_ivec_wrapper global_indices(MemorySpace m) const { return reshape(_proj.read(m), ndof); }
 
         /// @brief project H1Space3D vector to TraceSpace3D vector
         /// @param x DEVICE. H1Space3D vector
@@ -136,10 +111,7 @@ namespace cuddh
         void orth(double *x) const;
 
         /// @brief returns the global H1Space3D
-        const H1Space3D &h1_space() const
-        {
-            return fem;
-        }
+        const H1Space3D &h1_space() const { return fem; }
 
     private:
         const H1Space3D &fem;
